42f95a44e140ad712baa82b39a93a8abf4abf39e
Matt Dietz <matthew.dietz@gmail.com>
2019-01-18 10:26:56 -0600

    Generates composes
    A bunch of changes, too many to count. Still heavily WIP, needs to be
    refactored like mad, but it's getting the job done.

--- vessel/shell.py
+++ vessel/shell.py
@@ -1,4 +1,7 @@
+import json
 import os
+import shlex
+import subprocess
 import sys
 
 import click
@@ -8,6 +11,8 @@ import toml
 VESSEL_ROOT = os.path.expanduser("~/.vessel")
 VESSEL_CONFIG = "{}/config.toml".format(VESSEL_ROOT)
 DEFAULT_SSH_KEY = os.path.expanduser("~/.ssh")
+CONFIG_FILE = "config.toml"
+COMPOSE_FILE = "docker-compose.yaml"
 
 # This implicitly supports any other language. We just check for
 # $LANG_base_*
@@ -20,19 +25,27 @@ DEFAULT_CONFIG = {
         "base_image": "",
         "base_dockerfile": ""
     },
+    "supress_warnings": False,
     "default_backend": "docker-compose",
+    "set_project_path_to_cwd": True,
     "ssh_key_path": DEFAULT_SSH_KEY if os.path.exists(DEFAULT_SSH_KEY) else ""
 }
 
+
+# TODO Templatize this with assloads of comments
 DEFAULT_PROJECT_CONFIG = {
+    "name": "",
     "image": "",
     "custom_build": False,
-    "volumes": [],
-    "ports": [],
+    "volumes": {},
+    "ports": {},
     "language": "",
+    "network_mode": "",
     "entrypoint": "",
+    "command": "",
     "mount_ssh_keys": False,
     "default_backend": "",
+    "dependencies": [],
     "environment": {
         "build": {},
         "host": {},
@@ -46,8 +59,111 @@ jinja_env = jinja2.Environment(
 )
 
 
+class Service(object):
+    REQUIRED = ["name", "image"]
+
+    def __init__(self, **kwargs):
+        for key in self.REQUIRED:
+            if key not in kwargs:
+                raise Exception("Missing required config key '{}'".format(key))
+        self.command = kwargs.get("command")
+        self.custom_build = kwargs.get("custom_build")
+        self.default_backend = kwargs.get("default_backend")
+        self.dependencies = kwargs.get("dependencies")
+        self.entrypoint = kwargs.get("entrypoint")
+        self.env_vars = kwargs.get("env_vars")
+        self.image = kwargs.get("image")
+        self.mount_ssh_keys = kwargs.get("mount_ssh_keys")
+        self.name = kwargs.get("name")
+        self.network_mode = kwargs.get("network_mode")
+        self.ports = kwargs.get("ports")
+        self.volumes = kwargs.get("volumes")
+
+        if "environment" in kwargs:
+            self.host_env_vars = kwargs["environment"]["host"]
+            self.build_env_vars = kwargs["environment"]["build"]
+            self.container_env_vars = kwargs["environment"]["container"]
+
+    @classmethod
+    def escape_host_envvars(cls, envvar):
+        prev = None
+        found_var = False
+        escaped = False
+        var_start = -1
+        strbuffer = []
+        if not isinstance(envvar, str):
+            return envvar
+
+        for i, c in enumerate(envvar):
+            if found_var:
+                c_val = ord(c)
+                if (c_val >= 48 and c_val <= 57) or \
+                        (c_val >= 65 and c_val <= 90) or \
+                        (c_val >= 97 and c_val <= 122) or \
+                        (c_val == 95):
+                    pass
+                else:
+                    # Found the end of the var
+                    var = envvar[var_start+1:i]
+                    strbuffer.extend("{{{}}}".format(var))
+                    var_start = -1
+                    found_var = False
+
+            if not found_var:
+                strbuffer.append(c)
+
+            if c == '$':
+                if escaped:
+                    escaped = False
+                    found_var = False
+                else:
+                    found_var = True
+                    var_start = i
+            elif c == "\\":
+                if escaped:
+                    escaped = False
+                else:
+                    escaped = True
+
+        # We got to the end of the string
+        if found_var:
+            var = envvar[var_start+1:]
+            strbuffer.extend("{{{}}}".format(var))
+
+        return "".join(strbuffer)
+
+
 def _project_path(project):
     return "{}/{}".format(VESSEL_ROOT, project)
+
+
+def _project_config_path(project):
+    return "{}/{}".format(_project_path(project), CONFIG_FILE)
+
+
+def _project_exists(project):
+    return os.path.exists(_project_path(project))
+
+
+def _project_compose_path(project):
+    return "{}/{}".format(_project_path(project), COMPOSE_FILE)
+
+
+def _enforce_host_vars(cfg):
+    # TODO Maybe have special known vars. PROJECT_PATH if unset could
+    #      be set to CWD
+    for k, v in cfg.get("environment", {}).get("host", {}).items():
+        if k not in os.environ:
+            if v != "":
+                os.environ.setdefault(k, v)
+            else:
+                click.echo("'{}' must be set in your environment before "
+                           "the project can be run".format(k))
+                sys.exit(1)
+
+
+def _set_workdir_var():
+    os.environ.setdefault("WORKDIR", os.getcwd())
 
 
 def _check_initialized():
@@ -56,13 +172,74 @@ def _check_initialized():
         sys.exit(1)
 
 
+def _enforce_editor():
+    if ("VISUAL" not in os.environ and "EDITOR" not in os.environ):
+        click.echo("$VISUAL or $EDITOR must be set to use this command")
+        sys.exit(1)
+    if "EDITOR" in os.environ:
+        return os.environ["EDITOR"]
+    elif "VISUAL" in os.environ:
+        return os.environ["VISUAL"]
+
+
+def _generate_compose(projects):
+    try:
+        svc_cfg = [Service(**p) for p in projects]
+    except Exception as e:
+        sys.exit(e)
+    template = jinja_env.get_template("docker-compose.tpl")
+    return template.render(services=svc_cfg)
+
+
+def _pretty_json(blob):
+    return json.dumps(blob, sort_keys=True, separators=(", ", ": "), indent=4)
+
+
+def _pretty_toml(blob, outer=""):
+    for k, v in blob.items():
+        if isinstance(v, dict):
+            k_formatted = k
+            if outer:
+                k_formatted = "{}.{}".format(outer, k)
+                click.echo("[{}]".format(k_formatted))
+            _pretty_toml(v, outer=k_formatted)
+        else:
+            v_formatted = v
+            if isinstance(v, str):
+                v_formatted = '"{}"'.format(v)
+            click.echo("{} = {}".format(k, v_formatted))
+
+
+def _load_config(project=None):
+    if project:
+        with open(_project_config_path(project), 'r') as f:
+            return toml.load(f)
+    with open(VESSEL_CONFIG, 'r') as f:
+        return toml.load(f)
+
+
+def _discover_projects(root_project):
+    services = {}
+    cfg = _load_config(root_project)
+    services[cfg["name"]] = cfg
+    for project in cfg.get("dependencies", []):
+        sub_services = _discover_projects(project)
+        services.update(sub_services)
+    return services
+
+
 class CLIRoot(object):
     def __init__(self, debug=False):
         self._debug = debug
+        self._config = _load_config()
 
     @property
     def debug(self):
         return self._debug
+
+    @property
+    def config(self):
+        return self._config
 
 
 @click.group()
@@ -70,6 +247,11 @@ class CLIRoot(object):
 @click.pass_context
 def vessel(ctx, debug):
     ctx.obj = CLIRoot(debug)
+    if ("VISUAL" not in os.environ and
+        "EDITOR" not in os.environ and
+        not ctx.obj.config.get("suppress_warnings", False)):
+        click.echo("WARNING: You need to set your $VISUAL or $EDITOR "
+                   "environment variables to use config editing commands")
 
 
 # Commands I have been known to run:
@@ -119,54 +301,170 @@ def create(ctx, project, language):
 
     # TODO Load vrssel config before the command runs
     proj_config.update({
+        "name": project,
         "language": language,
         "default_backend": DEFAULT_CONFIG["default_backend"]
     })
+
+    proj_config["volumes"] = {
+        "{}/data".format(proj_path): "",
+        "$WORKDIR": "/CONTAINER_MOUNT_PATH/{}".format(project)
+    }
+
+    if language and language.strip().lower() == "python":
+        proj_config["volumes"] = {
+            "$HOME/.vessel/{}/venv".format(project): "/root/.virtualenvs"
+        }
+        proj_config["environment"]["host"]["PROJECT_PATH"] = "PathToPackage"
+
     with open("{}/config.toml".format(proj_path), 'w') as f:
         f.write(toml.dumps(proj_config))
 
-    # TODO This could move into a language specific driver later
-    if isinstance(language, str) and language.lower() == "python":
-        os.mkdir("{}/venv".format(proj_path))
+
+@vessel.command()
+@click.pass_context
+def list(ctx):
+    _check_initialized()
+    for root, dirs, files in os.walk(VESSEL_ROOT):
+        if root == VESSEL_ROOT:
+            continue
+        if CONFIG_FILE in files:
+            click.echo(os.path.split(root)[-1])
 
 
 @vessel.command()
 @click.argument("project")
 @click.pass_context
-def config(ctx, project):
-    pass
+def show(ctx, project):
+    # TODO Probably emove this later. Just for dev.
+    _check_initialized()
+    if not _project_exists(project):
+        click.echo("No project named '{}' exists. "
+                   "You should create one!".format(project))
+        sys.exit(1)
+    click.echo(_pretty_toml(_load_config(project)))
 
 
 @vessel.command()
+@click.argument("project")
+@click.pass_context
+def edit(ctx, project):
+    _check_initialized()
+    edit_cmd = _enforce_editor()
+    if not _project_exists(project):
+        click.echo("No project named '{}' exists. "
+                   "You should create one!".format(project))
+        sys.exit(1)
+    cmd = shlex.split("{} {}".format(edit_cmd, _project_config_path(project)))
+    subprocess.Popen(cmd).wait()
+
+
+@vessel.command()
+@click.pass_context
+def config(ctx):
+    _check_initialized()
+    edit_cmd = _enforce_editor()
+    cmd = shlex.split("{} {}".format(edit_cmd, VESSEL_CONFIG))
+    subprocess.Popen(cmd).wait()
+
+
+@vessel.command()
+@click.argument("project")
+@click.pass_context
+def up(ctx, project):
+    _check_initialized()
+    if not _project_exists(project):
+        click.echo("No project named '{}' exists. "
+                   "You should create one!".format(project))
+        sys.exit(1)
+    _set_workdir_var()
+
+    # Dis gon git recursive
+    cfg = _load_config(project)
+    _enforce_host_vars(cfg)
+    compose_path = _project_compose_path(project)
+    projects = _discover_projects(project).values()
+    with open(compose_path, 'w') as f:
+        f.write(_generate_compose(projects))
+
+    cmd = shlex.split("docker-compose -f {} up -d".format(compose_path))
+    subprocess.Popen(cmd)
+
+
+@vessel.command()
+@click.argument("project")
+@click.argument("command")
+@click.pass_context
+def run(ctx, project, command=None):
+    _check_initialized()
+    if not _project_exists(project):
+        click.echo("No project named '{}' exists. "
+                   "You should create one!".format(project))
+        sys.exit(1)
+    _set_workdir_var()
+
+    # Dis gon git recursive
+    cfg = _load_config(project)
+    _enforce_host_vars(cfg)
+    compose_path = _project_compose_path(project)
+    projects = _discover_projects(project).values()
+    with open(compose_path, 'w') as f:
+        f.write(_generate_compose(projects))
+
+    cmd = shlex.split("docker-compose -f {} run {}".format(compose_path,
+                                                           command))
+    subprocess.Popen(cmd)
+
+
+@vessel.command()
+@click.argument("project")
 @click.pass_context
 def export(ctx, project):
-    pass
+    _check_initialized()
+    if not _project_exists(project):
+        click.echo("No project named '{}' exists. "
+                   "You should create one!".format(project))
+        sys.exit(1)
+    _set_workdir_var()
+
+    # Dis gon git recursive
+    cfg = _load_config(project)
+    _enforce_host_vars(cfg)
+    compose_path = _project_compose_path(project)
+    projects = _discover_projects(project).values()
+    compose = _generate_compose(projects)
+    with open(compose_path, 'w') as f:
+        f.write(compose)
+    click.echo(compose)
+    
+
+@vessel.command()
+@click.argument("project")
+@click.pass_context
+def logs(ctx, project):
+    _check_initialized()
+    if not _project_exists(project):
+        click.echo("No project named '{}' exists. "
+                   "You should create one!".format(project))
+        sys.exit(1)
+
+    # Dis gon git recursive
+    compose_path = _project_compose_path(project)
+    cmd = shlex.split("docker-compose -f {} logs".format(compose_path))
+    subprocess.Popen(cmd)
 
 
 @vessel.command()
 @click.argument("project")
 @click.pass_context
-def run(ctx, project):
-    generate_compose()
+def kill(ctx, project):
+    _check_initialized()
+    if not _project_exists(project):
+        click.echo("No project named '{}' exists. "
+                   "You should create one!".format(project))
+        sys.exit(1)
 
-
-class Service(object):
-    def __init__(self):
-        self.name = ""
-        self.env_vars = {}
-        self.dependencies = []
-        self.image = ""
-        self.network_mode = ""
-        self.volumes = {}
-
-
-def generate_compose():
-    services = []
-    service = Service()
-    service.name = "argus"
-    service.image = "debian"
-    service.env_vars["ARGUS_WORKSPACE"] = "./argus"
-    service.volumes["${HOME}/.vessel/argus/venv"] = "/root/.virtualenvs"
-    services.append(service)
-    template = jinja_env.get_template("docker-compose.tpl")
-    click.echo(template.render(services=services))
+    # Dis gon git recursive
+    compose_path = _project_compose_path(project)
+    cmd = shlex.split("docker-compose -f {} kill".format(compose_path))
+    subprocess.Popen(cmd)
--- vessel/templates/docker-compose.tpl
+++ vessel/templates/docker-compose.tpl
@@ -3,33 +3,40 @@ version: '3'
 services:
   {%- for service in services %}
   {{ service.name }}:
-      {%- if service.dependencies %}
-      depends_on:
-      {%- for dep in service.dependencies %}
-        - {{ dep }}
-      {% endfor %}
-      {%- endif %}
-    {%- if service.env_vars %}
+    {%- if service.dependencies %}
+    depends_on:
+    {%- for dep in service.dependencies %}
+      - {{ dep }}
+    {%- endfor %}
+    {%- endif %}
+    {%- if service.container_env_vars %}
     environment:
-    {%- for name, value in service.env_vars.items() %}
-      {{ name }}: {{ value }}
+    {%- for name, value in service.container_env_vars.items() %}
+      {{ name }}: {{ service.escape_host_envvars(value) }}
     {%- endfor %}
     {%- endif %}
     {%- if service.command %}
     command: {{ service.command }}
+    {%- elif service.entrypoint %}
+    entrypoint: {{ service.entrypoint }}
     {%- endif %}
     image: {{ service.image }}
-    {{- service.network_mode and "network_mode:"}} {{ service.network_mode }}
+    {%- if service.network_mode %}
+    network_mode: {{ service.network_mode }}
+    {%- endif %}
+
     {%- if service.ports %}
     ports:
     {%- for source, dest in service.ports.items() %}
-      - {{ source }}: {{ dest }}
+      - "{{ source }}:{{ dest }}"
     {%- endfor %}
     {%- endif %}
     {%- if service.volumes %}
     volumes:
     {%- for source, dest in service.volumes.items() %}
-      - {{ source }}: {{ dest }}
+    {%- if source != "" and dest != "" %}
+      - {{ service.escape_host_envvars(source) }}:{{ service.escape_host_envvars(dest) }}
+    {%- endif %}
     {%- endfor %}
     {%- endif %}
     {%- endfor %}
